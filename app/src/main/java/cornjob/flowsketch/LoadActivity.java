6909a8b2fe94a8c085b3348b441abcaa6da439c6
Mark McCulloh <Mark.McCulloh@gmail.com>
2016-11-03 15:13:45 -0400

    init with alex code, mostly works

--- app/src/main/java/cornjob/flowsketch/LoadActivity.java
+++ app/src/main/java/cornjob/flowsketch/LoadActivity.java
@@ -4,17 +4,17 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+public class LoadActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_load);
 
-        //setting toolbar
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
 }
